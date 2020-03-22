2ff273f4486de13819465ea58031d9a3644502cf
pntigurirwa <pntigurirwa@gmail.com>
2020-03-21 22:13:57 -0500

    Using Console

---
+++ app.js
@@ -0,0 +1,23 @@
+// Log to console 
+console.log('Hello World');
+console.log(123);
+console.log(true);
+var greetings= 'Hello';
+console.log(greetings);
+console.log([1,2,3,4,5]);
+console.log({a:1, b:2});
+console.error('this is some error');
+console.clear();
+console.warn('this is a warning');
+console.time('Hello');
+    console.log('Hello World');
+    console.log('Hello World');
+    console.log('Hello World');
+    console.log('Hello World');
+    console.log('Hello World');
+    console.log('Hello World');
+    console.log('Hello World');
+    console.log('Hello World');
+console.timeEnd('Hello');
+
+/*Multi Line Comments*/

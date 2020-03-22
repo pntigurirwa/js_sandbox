b2e313619869991021df47aa3305dc9a0787cc24
pntigurirwa <pntigurirwa@gmail.com>
2020-03-21 22:32:29 -0500

    Variables

--- app.js
+++ app.js
@@ -21,3 +21,46 @@ console.time('Hello');
 console.timeEnd('Hello');
 
 /*Multi Line Comments*/
+
+//var, let, const
+// var name ='John Doe';
+// console.log (name);
+// name='Steve Smith';
+// console.log(name);
+// //Init var
+// var greeting;
+// console.log(greeting);
+// greeting ='Hello';
+// console.log(greeting);
+
+// //Letter, numbers, _, $
+// //can not start with number
+
+// //Multi wor vars
+// var firstName='John'; //Camel case
+// var first_name ='Sara'; //Underscore
+// var FirstName = 'Tom'; //Pascal case
+
+//LET
+// let name ='John Doe';
+// console.log (name);
+// name='Steve Smith';
+// console.log(name);
+
+// CONST 
+// can not be reassign
+//HAve to assign a value
+
+const person = {
+    name: 'John',
+    age: 30
+
+}
+person.name= 'Sara';
+person.age= 32;
+// console.log(person);
+
+const numbers = [1,2,3,4,5,6];
+numbers.push(7);
+console.log(numbers);
+

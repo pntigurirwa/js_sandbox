d475b1e46ebfd11398009e1a665d49cf392fcd13
pntigurirwa <pntigurirwa@gmail.com>
2020-05-26 12:22:44 -0500

    ES6 Promises

--- Easy_HTTP(Ajax with Callbacks)/app.js
+++ Easy_HTTP(Ajax with Callbacks)/app.js
@@ -1,11 +1,62 @@
 const http = new easyHTTP();
 
-// Get posts
+// // Get posts
 
-http.get("https://jsonplaceholder.typicode.com/posts", function (err, posts) {
+// http.get("https://jsonplaceholder.typicode.com/posts", function (err, posts) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(posts);
+//   }
+// });
+
+// // get Single Post
+// http.get("https://jsonplaceholder.typicode.com/posts/3", function (err, post) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(post);
+//   }
+// });
+
+// Create data
+const data = {
+  title: "Custom Post",
+  body: "This is a custom post",
+};
+
+// Create Post
+// http.post("https://jsonplaceholder.typicode.com/posts", data, function (
+//   err,
+//   post
+// ) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(post);
+//   }
+// });
+
+// Update post
+// http.put("https://jsonplaceholder.typicode.com/posts/1", data, function (
+//   err,
+//   post
+// ) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(post);
+//   }
+// });
+
+// Delete post
+http.delete("https://jsonplaceholder.typicode.com/posts/1", function (
+  err,
+  response
+) {
   if (err) {
     console.log(err);
   } else {
-    console.log(posts);
+    console.log(response);
   }
 });
